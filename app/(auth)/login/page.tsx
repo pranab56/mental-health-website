4f4f3f3358c447bd11177832b4f5412862ed47a9
pranab56 <pronabhalder53@gmail.com>
2026-02-09 15:11:08 +0600

    all ok

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -145,6 +145,12 @@ export default function LoginPage() {
               )}
             </Button>
           </form>
+          <div className="text-center mt-4 text-sm text-gray-500">
+            Don't have an account?{" "}
+            <Link href="/signup" className="text-[#5aa8a9] hover:text-[#4a8a8b] font-medium">
+              Register
+            </Link>
+          </div>
         </div>
       </div>
     </div>
